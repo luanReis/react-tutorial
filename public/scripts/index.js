daffbbf4e7edf154e2aa8fed1c24a8060852fc4b
Luan Reis <luan.rpsantos@gmail.com>
2016-06-28 18:43:58 -0300

    Remove hard coded data to use fetched data from server instead

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,14 +1,25 @@
-var data = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data}/>
+        <CommentList data={this.state.data}/>
         <CommentForm/>
       </div>
     );
@@ -56,6 +67,6 @@ var CommentForm = React.createClass({
 });
 
 ReactDOM.render(
-  <CommentBox data={data}/>,
+  <CommentBox url="/api/comments"/>,
   document.getElementById('content')
 );
